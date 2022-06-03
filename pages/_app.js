06724f60815af6c3a9c06a402155b7ac777300e5
Ryo Hilmi <ryodm.74@gmail.com>
2022-06-03 22:14:15 +0700

    feat: add material ui theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import "../styles/globals.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Poppins"].join(","),
+    fontSize: 14,
+    fontWeightMedium: 700,
+  },
+  components: {},
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
