b649c1f7995f50fbe23992d80c8c21e45f1fccbf
ryohilmi <ryodm.74@gmail.com>
2022-06-05 18:47:40 +0700

    feat: add karyawan api

---
+++ pages/api/karyawan/index.js
@@ -0,0 +1,14 @@
+const pool = require("../../../src/db");
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      const categories = await pool.query(`SELECT * FROM karyawan`);
+      res.json(categories.rows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    // Handle any other HTTP method
+  }
+}
