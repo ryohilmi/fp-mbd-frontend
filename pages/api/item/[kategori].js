8f00e3bd9ffba8f4a98ba046cba35f17ac601e72
ryohilmi <ryodm.74@gmail.com>
2022-06-05 18:48:10 +0700

    feat: update item api

--- pages/api/item/[kategori].js
+++ pages/api/item/[kategori].js
@@ -1,12 +1,14 @@
-const pool = require('../../../src/db');
+const pool = require("../../../src/db");
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     const { kategori } = req.query;
 
     try {
       const allTodos = await pool.query(
-        `SELECT item.*, kat_nama FROM item JOIN KATEGORI ON item.kat_id = kategori.kat_id WHERE item.kat_id = '${kategori}'`
+        `SELECT item.*, kat_nama FROM item 
+        JOIN KATEGORI ON item.kat_id = kategori.kat_id
+        WHERE item.kat_id = '${kategori}'`
       );
       res.json(allTodos.rows);
     } catch (err) {
