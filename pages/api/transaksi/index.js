9a9a84cf4d4569ba55113fadd54339ae7165539e
ryohilmi <ryodm.74@gmail.com>
2022-06-06 19:17:52 +0700

    feat: add get method for transaction

--- pages/api/transaksi/index.js
+++ pages/api/transaksi/index.js
@@ -1,7 +1,17 @@
 const pool = require("../../../src/db");
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
+  if (req.method === "GET") {
+    try {
+      const categories = await pool.query(`
+        SELECT * FROM transaksi_pembeli
+        JOIN customer ON transaksi_pembeli.cust_id = customer.cust_id
+      `);
+      res.json(categories.rows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else if (req.method === "POST") {
     try {
       console.log(req.body);
       res.json(req.body);
