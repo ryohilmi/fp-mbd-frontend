c175f849c6ea0abb6896d678406103588464f27d
ryohilmi <ryodm.74@gmail.com>
2022-06-06 21:39:59 +0700

    feat: add karyawan post api

--- pages/api/karyawan/index.js
+++ pages/api/karyawan/index.js
@@ -8,6 +8,17 @@ export default async function handler(req, res) {
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
+  } else if (req.method === "POST") {
+    const { nama } = req.body;
+
+    try {
+      const status = await pool.query(
+        `INSERT INTO karyawan VALUES (NEXTVAL('ID_KATEGORI_SEQ'), $1)`,
+        [nama]
+      );
+      console.log(status);
+      res.json(req.body);
+    } catch (err) {}
   } else {
     // Handle any other HTTP method
   }
