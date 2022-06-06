b96b47f28aa3e4a84452db385a41d69802e0ab77
Fathur45 <fathurahman450@gmail.com>
2022-06-06 23:06:54 +0700

    feat: add insert transaction

--- pages/api/transaksi/index.js
+++ pages/api/transaksi/index.js
@@ -13,9 +13,29 @@ export default async function handler(req, res) {
     }
   } else if (req.method === "POST") {
     try {
-      console.log(req.body);
-      res.json(req.body);
-    } catch (err) {}
+      const { karyawan_id, customer_id, detail } = req.body;
+      const status = await pool.query(
+        `INSERT INTO transaksi_pembeli VALUES (NEXTVAL('ID_TRANSAKSI_PEMBELI_SEQ'), $1, $2, NOW(), NULL)`,
+        [karyawan_id, customer_id]
+      );
+
+      detail.forEach(async (item) => {
+        try {
+          const resolt = await pool.query(
+            `INSERT INTO kwitansi_pembeli VALUES ($1, (SELECT last_value FROM ID_TRANSAKSI_PEMBELI_SEQ), $2)`,
+            [item.id, item.total]
+          );
+
+          res.status(201).json(resolt);
+        } catch (err) {
+          res.status(569).json(err);
+        }
+
+        console.log(res);
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   } else {
     // Handle any other HTTP method
   }
