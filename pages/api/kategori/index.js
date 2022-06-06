c6bdd38d92c62e87af928726ca00ef4c8dea6bb4
ryohilmi <ryodm.74@gmail.com>
2022-06-06 18:37:41 +0700

    feat: add POST handler for categories

--- pages/api/kategori/index.js
+++ pages/api/kategori/index.js
@@ -1,13 +1,24 @@
-const pool = require('../../../src/db');
+const pool = require("../../../src/db");
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     try {
       const categories = await pool.query(`SELECT * FROM kategori`);
       res.json(categories.rows);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
+  } else if (req.method === "POST") {
+    const { nama } = req.body;
+
+    try {
+      const status = await pool.query(
+        `INSERT INTO kategori VALUES (NEXTVAL('ID_KATEGORI_SEQ'), $1)`,
+        [nama]
+      );
+      console.log(status);
+      res.json(req.body);
+    } catch (err) {}
   } else {
     // Handle any other HTTP method
   }
