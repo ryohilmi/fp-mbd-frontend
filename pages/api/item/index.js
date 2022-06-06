47c62aa478c557a24c8ff65c60526b50ae0e9690
ryohilmi <ryodm.74@gmail.com>
2022-06-06 21:14:59 +0700

    feat: add post method for item

--- pages/api/item/index.js
+++ pages/api/item/index.js
@@ -1,12 +1,26 @@
-const pool = require('../../../src/db');
+const pool = require("../../../src/db");
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     try {
       const allTodos = await pool.query(
-        'SELECT item.*, kat_nama FROM item JOIN KATEGORI ON item.kat_id = kategori.kat_id'
+        "SELECT item.*, kat_nama FROM item JOIN KATEGORI ON item.kat_id = kategori.kat_id"
       );
       res.json(allTodos.rows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else if (req.method === "POST") {
+    console.log(req.body);
+    const { nama, harga, category, deskripsi } = req.body;
+
+    try {
+      const status = await pool.query(
+        `INSERT INTO item VALUES (NEXTVAL('ID_ITEM_SEQ'), $1, $2, $3, $4, $5);`,
+        [category, nama, 0, deskripsi, harga]
+      );
+      console.log(status);
+      res.json(req.body);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
